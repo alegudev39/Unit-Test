12c760c0495cfe868bf1b5cc68230c74c9442849
AleDev <aledev@example.com>
2024-05-31 17:22:31 +0200

    first commit

---
+++ src/main/java/com/dblibproject/dblibproject/DblibprojectApplication.java
@@ -0,0 +1,13 @@
+package com.dblibproject.dblibproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DblibprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DblibprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dblibproject/dblibproject/DblibprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dblibproject.dblibproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DblibprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
